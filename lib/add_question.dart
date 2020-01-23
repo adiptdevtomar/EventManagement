86b3ac28a6ebed561ab23acd5142b8394dd9ac93
adipt dev tomar <adipt.dev.tomar@gmail.com>
2020-01-23 19:36:36 +0530

    Add Screen

---
+++ lib/add_question.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddQues extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Add Question"),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
